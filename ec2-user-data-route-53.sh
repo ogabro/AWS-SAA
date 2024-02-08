fe9b57c215e4e19d30872466132d1efc3c9c74ff
ogabro <138379480+ogabro@users.noreply.github.com>
2024-02-08 10:41:27 +0200

    Add files via upload

---
+++ ec2-user-data-route-53.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "<h1>Hello World from $(hostname -f)in AZ $EC2_AVAIL_ZONE </h1>" > /var/www/html/index.html
